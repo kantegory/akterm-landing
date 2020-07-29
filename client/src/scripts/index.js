d8878c059a880e6d79804d0cecb7bf5a43f14e6b
kantegory <davidobryakov@gmail.com>
2020-07-29 21:26:24 +0300

    Add configs

--- client/src/scripts/index.js
+++ client/src/scripts/index.js
@@ -1,18 +1,25 @@
-const dir = "/home/kantegory/work/arkterm/images/";
 const fs = require('fs');
+const conf = require('ini');
 const pug = require('pug');
 
-fs.readdir(dir, function(err, files) {
+
+let config = conf.parse(fs.readFileSync('/home/kantegory/work/arkterm/client/config/config.ini', 'utf-8'));
+
+let picDir = config.common.pic_dir;
+let viewsDir = config.common.views_dir;
+let buildDir = config.common.build_dir;
+
+fs.readdir(picDir, function(err, files) {
     let html = pug.renderFile(
-        '/home/kantegory/work/arkterm/client/src/views/index.pug',
+        `${viewsDir}/index.pug`,
         {
-            directory: dir,
+            directory: picDir,
             images: files
         }
     )
 
     // do something with html
     console.log(html);
-    fs.writeFileSync('/home/kantegory/work/arkterm/client/dist/index.html', html);
+    fs.writeFileSync(`${buildDir}/index.html`, html);
 });
 
--- server/index.js
+++ server/index.js
@@ -1,19 +1,25 @@
 const http = require('http');
 const formidable = require('formidable');
 const fs = require('fs');
+const conf = require('ini');
 const shell = require('shelljs');
+
+let config = conf.parse(fs.readFileSync('config/config.ini', 'utf-8'));
+
+let picDir = config.common.pic_dir;
+let scriptsDir = config.common.scripts_dir;
 
 http.createServer(function (req, res) {
   if (req.url == '/fileupload') {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
       var oldpath = files.filetoupload.path;
-      var newpath = '../images/' + files.filetoupload.name;
+      var newpath = picDir + files.filetoupload.name;
       fs.rename(oldpath, newpath, function (err) {
         if (err) throw err;
         res.write('File uploaded and moved!');
         res.end();
-        shell.exec('scripts/update.sh');
+        shell.exec(`${scriptsDir}update.sh`);
       });
  });
   } else {
