70ec703a85b144cbb06d013a73af7e9e0461bcca
David Dobryakov <davidobryakov@gmail.com>
2020-08-02 15:03:24 +0300

    Change picDir, put styles into separate file

--- server/scripts/update.sh
+++ server/scripts/update.sh
@@ -1,3 +1,3 @@
 node /var/www/html/arkterm/client/src/scripts/index.js
 cp /var/www/html/arkterm/client/src/scripts/mailer.js /var/www/html/arkterm/client/dist/scripts/
-
+cp /var/www/html/arkterm/client/src/styles/index.css /var/www/html/arkterm/client/dist/styles/
