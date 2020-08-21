11db77d5cbf676095dedd281adf5180e3faf5068
kantegory <davidobryakov@gmail.com>
2020-08-21 14:24:41 +0300

    Add closeBtn to gallery

--- client/src/scripts/portfolio.js
+++ client/src/scripts/portfolio.js
@@ -1,13 +1,20 @@
 Array.from(document.querySelectorAll('.portfolio__image')).map((img) => {
   img.addEventListener('click', () => {
     let preview = document.querySelector('.portfolio__preview');
+    let closeBtn = document.querySelector('.portfolio__close');
 
     preview.innerHTML = img.outerHTML;
 
     preview.style.visibility = 'visible';
+    closeBtn.style.visibility = 'visible';
 
-    preview.addEventListener('click', () => {
+    // lock scroll
+    // document.querySelector('html').style.overflowY = 'hidden';
+
+    closeBtn.addEventListener('click', () => {
       preview.style.visibility = 'hidden';
+      closeBtn.style.visibility = 'hidden';
+      // document.querySelector('html').style.overflowY = 'scroll';
       preview.innerHTML = "";
     })
   })
