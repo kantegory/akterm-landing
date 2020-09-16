39935afbc42be587d435145b87a6d51667433612
kantegory <davidobryakov@gmail.com>
2020-09-16 20:31:37 +0300

    Update update.sh script

--- server/scripts/update.sh
+++ server/scripts/update.sh
@@ -1,5 +1,4 @@
 node /var/www/html/arkterm/client/src/scripts/index.js
 cp /var/www/html/arkterm/client/src/scripts/*.js /var/www/html/arkterm/client/dist/scripts/
 cp /var/www/html/arkterm/client/src/styles/*.css /var/www/html/arkterm/client/dist/styles/
-cp -r /var/www/html/arkterm/client/src/views/images/* /var/www/html/arkterm/client/dist/views/images/
-
+cp -r /var/www/html/arkterm/client/src/views/images /var/www/html/arkterm/client/dist/views/images
